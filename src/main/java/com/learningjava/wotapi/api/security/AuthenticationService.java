f92c4019b4ab3016d1267dbfc9463c5293491c9a
JaapvDijk <dijkvanjaap@hotmail.com>
2025-07-28 18:07:30 +0200

    Fix: new users get "user" role.

--- src/main/java/com/learningjava/wotapi/api/security/AuthenticationService.java
+++ src/main/java/com/learningjava/wotapi/api/security/AuthenticationService.java
@@ -3,25 +3,33 @@ package com.learningjava.wotapi.api.security;
 import com.learningjava.wotapi.api.model.dto.LoginRequest;
 import com.learningjava.wotapi.api.model.dto.RegisterUserRequest;
 import com.learningjava.wotapi.api.model.entity.User;
+import com.learningjava.wotapi.api.model.entity.Role;
+import com.learningjava.wotapi.api.repo.RoleRepository;
 import com.learningjava.wotapi.api.repo.UserRepository;
+import com.learningjava.wotapi.api.contant.Roles;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     private final PasswordEncoder passwordEncoder;
 
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationService(UserRepository userRepository,
+                                 RoleRepository roleRepository,
                                  AuthenticationManager authenticationManager,
                                  PasswordEncoder passwordEncoder) {
-        this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.authenticationManager = authenticationManager;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -30,6 +38,11 @@ public class AuthenticationService {
         user.setEmail(input.getEmail());
         user.setFullName(input.getFullName());
         user.setPassword(passwordEncoder.encode(input.getPassword()));
+
+        Role role = roleRepository.findByName(Roles.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("User role not found"));
+
+        user.setRoles(List.of(role));
 
         return userRepository.save(user);
     }
