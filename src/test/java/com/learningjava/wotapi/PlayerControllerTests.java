67a0c06c43a4afde06af0061799a66fe2225c01b
JaapvDijk <dijkvanjaap@hotmail.com>
2025-07-31 16:23:44 +0200

    Added tests for player/tanks

--- src/test/java/com/learningjava/wotapi/PlayerControllerTests.java
+++ src/test/java/com/learningjava/wotapi/PlayerControllerTests.java
@@ -1,44 +1,32 @@
 package com.learningjava.wotapi;
 
-import com.learningjava.wotapi.api.importer.TomatoImporter;
-
-import com.learningjava.wotapi.api.service.WargamingClient;
-import com.learningjava.wotapi.api.service.WargamingService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.*;
 
-//TODO: Tests for endpoints:
-// - Player/Info/{Id}
-// - Player/Tanks/{Id}
 @SpringBootTest
 @AutoConfigureMockMvc
-//@ImportAutoConfiguration(exclude = SecurityAutoConfiguration.class) //Required For admin, user controller tests?
 public class PlayerControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
     private final int playerId = 537793577;
     private final String playerName = "iyouxin";
-    private final int unknownId = 123456789;
 
-    //region SEARCH ENDPOINTS
     @Test
     public void testSearch_withValidInput_returnsOk() throws Exception {
-       var a = mockMvc.perform(get("/player/search")
+       mockMvc.perform(get("/player/search")
                         .param("name", playerName)
                         .param("region", "EU"))
                 .andExpect(status().isOk())
@@ -79,12 +67,9 @@ public class PlayerControllerTests {
                 .andExpect(status().isBadRequest())
                 .andExpect(content().string(containsString("Region must be either 'EU', 'NA' or 'ASIA'")));
     }
-    //endregion
 
-    //region INFO ENDPOINTS
     @Test
     public void testGetInfo_withValidId_returnsOk() throws Exception {
-
         mockMvc.perform(get("/player/info/{id}", playerId)
                         .param("region", "EU"))
                 .andExpect(status().isOk())
@@ -94,10 +79,31 @@ public class PlayerControllerTests {
 
     @Test
     public void testGetInfo_withUnknownId_returnsNotFound() throws Exception {
+        int unknownId = 123456789;
         //Act & Assert
         mockMvc.perform(get("/player/info/{id}", unknownId)
                         .param("region", "EU"))
                 .andExpect(status().isNotFound());
     }
-    //endregion
+
+    @Test
+    public void testGetTanks_withValidId_returnsOk() throws Exception {
+        mockMvc.perform(get("/player/tanks/{id}", playerId)
+                        .param("region", "EU"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", not(empty())))
+                .andExpect(jsonPath("$[0].tank_id", isA(Integer.class)))
+                .andExpect(jsonPath("$[0].tank_id", greaterThan(0)))
+                .andExpect(jsonPath("$[0].mark_of_mastery", allOf(greaterThanOrEqualTo(0), lessThanOrEqualTo(4))))
+                .andExpect(jsonPath("$[0].statistics", notNullValue()));
+    }
+
+    @Test
+    public void testGetTanks_withUnknownId_returnsNotFound() throws Exception {
+        int unknownId = 123456789;
+        //Act & Assert
+        mockMvc.perform(get("/player/tanks/{id}", unknownId)
+                        .param("region", "EU"))
+                .andExpect(status().isNotFound());
+    }
 }
