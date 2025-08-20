805a3a3bdd3d453a4cb66a14d54c9a078d452fdb
Jaap van Dijk <dijkvanjaap@hotmail.com>
2025-08-20 17:12:38 +0200

    updated utils

--- client/src/utils/StatUtils.tsx
+++ client/src/utils/StatUtils.tsx
@@ -1,7 +1,30 @@
-export function getPercentage(total?: number, matches?: number) {
-    if (!total || total <= 0) {  
-        return undefined;
-    }
+export function getPercentage(total?: number, matches?: number): number {
+  if (!total || total <= 0 || !matches) {  
+    return 0;
+  }
 
-    return matches && ((matches / total * 100).toFixed(2) + '%');
+  return Number(((matches / total) * 100).toFixed(2));
 }
+
+export const getWinrateVerdict = (winrate?: number): string => {
+  if (!winrate || winrate <= 0) return "unknown";
+
+  switch (true) {
+    case winrate <= 46:
+      return "very bad";
+    case winrate <= 47:
+      return "below average";
+    case winrate <= 49:
+      return "average";
+    case winrate <= 52:
+      return "good";
+    case winrate <= 54:
+      return "very good";
+    case winrate <= 56:
+      return "great";
+    case winrate < 65:
+      return "unicum";
+    default:
+      return "super unicum";
+  }
+};
