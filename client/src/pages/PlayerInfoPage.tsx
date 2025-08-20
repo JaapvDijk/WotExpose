41a4cb4b4760cfe0b90516e9a0242c4e9e707602
Jaap van Dijk <dijkvanjaap@hotmail.com>
2025-08-20 15:29:58 +0200

    added some grids

--- client/src/pages/PlayerInfoPage.tsx
+++ client/src/pages/PlayerInfoPage.tsx
@@ -1,11 +1,10 @@
 import { useParams } from "react-router-dom";
-import { AppBar, Box, Button, Card, CardContent, Typography, CircularProgress } from "@mui/material";
-import { Api, PlayerRequest, WoTPlayerInfoResponse } from '../__generated__/Api';
+import { Box, Card, CardContent, Typography, Skeleton } from "@mui/material";
+import { Api, PlayerRequest } from '../__generated__/Api';
 import { useQuery } from "@tanstack/react-query";
 import Grid from '@mui/material/Grid';
 import { timestampToDate } from "../utils/DateUtils";
-import { useState } from "react";
-import { getPercentage } from '../utils/NumberUtils';
+import { getPercentage } from '../utils/StatUtils';
 
 const api = new Api();
 
@@ -31,51 +30,92 @@ function PlayerInfoPage() {
     let tanks = tanksResult?.data?.data;
     
     return (
-        <div>
-            <AppBar position="static">
-                <Typography variant="h6" style={{ padding: 3 }}>
-                    { infoResult.isLoading ? (<CircularProgress size={20} style={{ marginRight: 8 }} />) 
-                    : infoResult.isError ? (<>Error: {(infoResult.error as Error).message}</>)
-                    : info ? (<>
-                        {info.nickname}#{info.account_id} - Rating: {info.global_rating} - 
-                        Winrate: {getPercentage(info.statistics?.all?.battles, info.statistics?.all?.wins)}
-                        <br/>Registered: {timestampToDate(info.created_at)} Last battle: {timestampToDate(info.last_battle_time)}
-                    </>)
-                    : ("No player info") }
-                </Typography>
-            </AppBar>
-
-            <Grid container spacing={2} style={{ padding: 20 }}>
-                <Grid size={{xs:12}}>
-                    <Card>
-                        <CardContent>
-                            <Typography variant="body2" color="textSecondary">
-                                tanks: { tanks?.first?.all?.explosion_hits }
-                            </Typography>
-                        </CardContent>
-                    </Card>
+        <>
+            <Grid container padding={2} spacing={2} alignItems="center">
+            {infoResult.isLoading ? (
+                <Skeleton variant="rectangular" height={100} />
+            ) : infoResult.isError ? (
+                <CardContent>
+                Could not load player info: {(infoResult.error as Error).message}
+                </CardContent>
+            ) : info ? (
+                <>
+                <Grid container spacing={2} size={{xs:7, md:10}} direction="row" alignItems="center">
+                    <Grid>
+                    <Typography variant="h6">
+                        {info.nickname}#{info.account_id}
+                    </Typography>
+                    </Grid>
+                    <Grid>
+                    <Typography variant="body1">
+                        Rating: <strong>{info.global_rating}</strong>
+                    </Typography>
+                    </Grid>
+                    <Grid>
+                    <Typography variant="body1">
+                        Winrate: <strong>{getPercentage(info.statistics?.all?.battles, info.statistics?.all?.wins)}%</strong>
+                    </Typography>
+                    </Grid>
                 </Grid>
 
-                <Grid size={{xs:12, sm:8}}>
-                    <Card>
-                        <Typography variant="h6">
-                            Lorem
-                        </Typography>
-                    </Card>
+                <Grid size={{xs:5, md:2}}>
+                    <Typography variant="caption">
+                    Registered: {timestampToDate(info.created_at)}
+                    </Typography>
+                    <br />
+                    <Typography variant="caption">
+                    Last battle: {timestampToDate(info.last_battle_time)}
+                    </Typography>
                 </Grid>
-
-                <Grid size={{xs:12, sm:4}}>
-                    <Box>
-                        <Typography variant="h6">
-                            Lorem
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary">
-                            Ipsum
-                        </Typography>
-                    </Box>
-                </Grid>
+                </>
+            ) : (
+                <CardContent>No player info</CardContent>
+            )}
             </Grid>
-        </div>
+
+            <Grid container spacing={2} gap={2}>
+            {tanksResult.isLoading ? (
+            <Skeleton variant="rectangular" height={100} />
+            ) : tanksResult.isError ? (
+                <CardContent>
+                    Could not load player info: {(tanksResult.error as Error).message}
+                </CardContent>
+            ) : tanks ? (
+                <> 
+                    <Grid size={{xs:12}}>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="body2" color="textSecondary">
+                                    tanks: {tanks?.[0]?.all?.shots ?? 0}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+
+                    <Grid size={{xs:12, sm:8}}>
+                        <Card>
+                            <Typography variant="h6">
+                                Lorem
+                            </Typography>
+                        </Card>
+                    </Grid>
+
+                    <Grid size={{xs:12, sm:4}}>
+                        <Box>
+                            <Typography variant="h6">
+                                Lorem
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary">
+                                Ipsum
+                            </Typography>
+                        </Box>
+                    </Grid>
+                </>
+            ) : (
+                <CardContent>No tanks info</CardContent>
+            )}
+            </Grid>
+        </>
     );
 }
 
