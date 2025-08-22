7c624aa27a56db912c1793699be1669de22013f1
Jaap van Dijk <dijkvanjaap@hotmail.com>
2025-08-22 13:02:38 +0200

    deleted

--- src/main/java/com/learningjava/wotapi/api/model/worldoftanks/dto/WoTPlayerTankStatsResponse.java
+++
@@ -1,6 +0,0 @@
-package com.learningjava.wotapi.api.model.worldoftanks.dto;
-
-import java.util.ArrayList;
-
-public class WoTPlayerTankStatsResponse extends ArrayList<WoTPlayerTankStatResponse> {
-}
