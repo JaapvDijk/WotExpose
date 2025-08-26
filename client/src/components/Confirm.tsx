31a9458731b532a40a2bb41fadc2e469b51ea403
Jaap van Dijk <dijkvanjaap@hotmail.com>
2025-08-26 18:19:03 +0200

    Added customizable confirm popup

--- client/src/components/Confirm.tsx
+++ client/src/components/Confirm.tsx
@@ -1,13 +1,36 @@
 import { confirmable, createConfirmation, type ConfirmDialogProps } from 'react-confirm';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Typography,
+} from '@mui/material';
 
-const MyDialog = ({ show, proceed, message }: ConfirmDialogProps<{ message: string }, boolean>) => (
-  <div className={`dialog-overlay ${show ? 'show' : 'hide'}`}>
-    <div className="dialog">
-      <p>{message}</p>
-      <button onClick={() => proceed(true)}>Yes</button>
-      <button onClick={() => proceed(false)}>No</button>
-    </div>
-  </div>
+const MyDialog = ({
+  show,
+  proceed,
+  message
+}: ConfirmDialogProps<{ message: string }, boolean>) => (
+  <Dialog
+    open={show}
+    onClose={() => proceed(false)}
+    maxWidth="xs"
+    fullWidth>
+    <DialogTitle>Confirm Action</DialogTitle>
+    <DialogContent>
+      <Typography>{message}</Typography>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={() => proceed(false)} color="inherit">
+        Cancel
+      </Button>
+      <Button onClick={() => proceed(true)} variant="contained" color="primary" autoFocus>
+        Yes
+      </Button>
+    </DialogActions>
+  </Dialog>
 );
 
 export const confirm = createConfirmation(confirmable(MyDialog));
