f686cc92082c51c665792c126bc8eaa05147922e
Jaap van Dijk <dijkvanjaap@hotmail.com>
2025-08-26 09:27:15 +0200

    moved vehicle related to seperate service

--- src/main/java/com/learningjava/wotapi/api/service/WargamingService.java
+++ src/main/java/com/learningjava/wotapi/api/service/WargamingService.java
@@ -2,14 +2,9 @@ package com.learningjava.wotapi.api.service;
 
 import com.learningjava.wotapi.api.factory.PlayerTankStatsFactory;
 import com.learningjava.wotapi.api.mapper.PlayerMapper;
-import com.learningjava.wotapi.api.mapper.TankStatMapper;
-import com.learningjava.wotapi.api.mapper.VehicleMapper;
 import com.learningjava.wotapi.api.model.dto.PlayerSearchResponse;
 import com.learningjava.wotapi.api.model.dto.PlayerTankStatsResponse;
 import com.learningjava.wotapi.api.model.worldoftanks.dto.WoTPlayerInfoResponse;
-import com.learningjava.wotapi.api.model.worldoftanks.dto.WoTVehicleResponse;
-import com.learningjava.wotapi.api.model.worldoftanks.dto.entity.Vehicle;
-import com.learningjava.wotapi.api.repo.VehicleRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,25 +14,14 @@ public class WargamingService {
 
     private final WargamingClient client;
     private final PlayerMapper playerMapper;
-    private final VehicleMapper vehicleMapper;
-    private final VehicleRepository vehicleRepository;
     private final PlayerTankStatsFactory playerTankStatsFactory;
 
     public WargamingService(WargamingClient client,
                             PlayerMapper mapper,
-                            VehicleMapper vehicleMapper,
-                            TankStatMapper tankStatMapper,
-                            VehicleRepository vehicleRepository,
                             PlayerTankStatsFactory playerTankStatsFactory) {
         this.client = client;
         this.playerMapper = mapper;
-        this.vehicleMapper = vehicleMapper;
-        this.vehicleRepository = vehicleRepository;
         this.playerTankStatsFactory = playerTankStatsFactory;
-    }
-
-    public List<WoTVehicleResponse> fetchVehicles() {
-        return client.getVehicles();
     }
 
     public List<PlayerSearchResponse> getPlayers(String name) {
@@ -54,15 +38,5 @@ public class WargamingService {
         var result = client.getPlayerTanks(id);
 
         return playerTankStatsFactory.from(result);
-    }
-
-    public void saveVehicles(List<WoTVehicleResponse> vehicles, String region)
-    {
-        vehicles.forEach(vehicle -> {
-            var result = vehicleMapper.toEntity(vehicle);
-            result.getId().setRegion(region);
-
-            vehicleRepository.save(result);
-        });
     }
 }
