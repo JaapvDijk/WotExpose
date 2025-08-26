a740987d4c91aee821d0d3651187262e5687de98
Jaap van Dijk <dijkvanjaap@hotmail.com>
2025-08-26 18:18:41 +0200

    moved gradient and height layout to container above

--- client/src/pages/PlayerInfoPage.tsx
+++ client/src/pages/PlayerInfoPage.tsx
@@ -33,86 +33,84 @@ function PlayerInfoPage() {
     const winrate = getPercentage(info?.statistics?.all?.battles, info?.statistics?.all?.wins);
     
     return (
-        <Container>
-            <Box sx={{ minHeight: 'calc(100vh - var(--footer-height, 0px))', background: 'linear-gradient(180deg, #1f1f1f, #121212)', p: 3 }}>
-                <Grid container padding={2} sx={{ minHeight: 125 }}>
-                {infoResult.isLoading ? (
-                    <Grid size={{xs:12}}>
-                        <Skeleton variant="rectangular" height="100%" />
-                    </Grid>
-                ) : infoResult.isError ? (
-                    <Grid size={{xs:12}}>
-                        Could not load player info: {(infoResult.error as Error).message}
-                    </Grid>
-                ) : info ? (
-                    <>
-                        <Grid container spacing={2} size={{xs:12, md:9}} direction="row" alignItems="center" 
-                        sx={{width:'100%', textAlign:'left', p: 3, mb: 1, borderRadius: 2, background: 'linear-gradient(135deg, #0c4763 0%, #292977 100%)', color: 'white'}}>
-                            <Grid>
-                                <Typography variant="h6" fontWeight="bold">
-                                {info.nickname}#{info.account_id}
-                                </Typography>
-                            </Grid>
-
-                            <Grid>
-                                <Typography variant="subtitle1">
-                                Rating: <strong>{info.global_rating}</strong> | Winrate: <strong>{winrate}%</strong>
-                                </Typography>
-                            </Grid>
-
-                            <Grid size={12}>
-                                <Typography variant="body1" textAlign="left">
-                                    This player performs <b style={{ textShadow: `0 0 0.6px black`, color: getWinrateXVMColour(winrate) }}>{getWinrateVerdict(winrate)}</b>, lets see why
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                        
-                        <Grid size={{xs:12, md:3}} sx={{ textAlign: { xs: 'center', md: 'right' }}}>
-                            <Typography variant="caption">
-                                Registered: {timestampToDate(info.created_at)}
-                            </Typography>
-                            <br />
-                            <Typography variant="caption">
-                                Last battle: {timestampToDate(info.last_battle_time)}
+        <Container sx={{ minHeight: 'calc(100vh - var(--footer-height, 0px))', background: "linear-gradient(180deg, #1f1f1f, #121212)", p: 3 }}>
+            <Grid container padding={2} sx={{ minHeight: 125 }}>
+            {infoResult.isLoading ? (
+                <Grid size={{xs:12}}>
+                    <Skeleton variant="rectangular" height="100%" />
+                </Grid>
+            ) : infoResult.isError ? (
+                <Grid size={{xs:12}}>
+                    Could not load player info: {(infoResult.error as Error).message}
+                </Grid>
+            ) : info ? (
+                <>
+                    <Grid container spacing={2} size={{xs:12, md:9}} direction="row" alignItems="center" 
+                    sx={{width:'100%', textAlign:'left', p: 3, mb: 1, borderRadius: 2, background: 'linear-gradient(135deg, #0c4763 0%, #292977 100%)', color: 'white'}}>
+                        <Grid>
+                            <Typography variant="h6" fontWeight="bold">
+                            {info.nickname}#{info.account_id}
                             </Typography>
                         </Grid>
-                    </>
-                ) : (
-                    <CardContent>No player info</CardContent>
-                )}
-                </Grid>
 
-                <Divider sx={{ my: 2 }} />
-
-                <Grid container spacing={2} sx={{ minHeight: 200 }}>
-                {tanksResult.isLoading ? (
-                    <Grid size={{xs:12}}>
-                        <Skeleton variant="rectangular" height="100%" />
-                    </Grid>
-                ) : tanksResult.isError ? (
-                    <Grid size={{xs:12}}>
-                        Could not load tank stats: {(tanksResult.error as Error).message}
-                    </Grid>
-                ) : tanks && tanks.totals?.battlesAll && tanks.totals?.battlesAll > 500 ? (
-                    <> 
-                        <Grid size={{xs:12}} sx={{ textAlign: 'left' }}>
-                            <Card>
-                                <CardContent>
-                                    <Typography variant="body2" color="textSecondary">
-                                        total: {tanks.totals?.battlesAll} <br/>
-                                    </Typography>
-                                    <Typography variant="body2" color="textSecondary">
-                                        most battles tank: {tanks?.data?.at(0)?.all?.battles}
-                                    </Typography>
-                                </CardContent>
-                            </Card>
+                        <Grid>
+                            <Typography variant="subtitle1">
+                            Rating: <strong>{info.global_rating}</strong> | Winrate: <strong>{winrate}%</strong>
+                            </Typography>
                         </Grid>
-                    </>
-                ) : (
-                    <CardContent>No tank stats found, or not enough battles (500+) {tanks?.totals?.battlesAll}</CardContent>
-                )}
+
+                        <Grid size={12}>
+                            <Typography variant="body1" textAlign="left">
+                                This player performs <b style={{ textShadow: `0 0 0.6px black`, color: getWinrateXVMColour(winrate) }}>{getWinrateVerdict(winrate)}</b>, lets see why
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    
+                    <Grid size={{xs:12, md:3}} sx={{ textAlign: { xs: 'center', md: 'right' }}}>
+                        <Typography variant="caption">
+                            Registered: {timestampToDate(info.created_at)}
+                        </Typography>
+                        <br />
+                        <Typography variant="caption">
+                            Last battle: {timestampToDate(info.last_battle_time)}
+                        </Typography>
+                    </Grid>
+                </>
+            ) : (
+                <CardContent>No player info</CardContent>
+            )}
+            </Grid>
+
+            <Divider sx={{ my: 2 }} />
+
+            <Grid container spacing={2} sx={{ minHeight: 200 }}>
+            {tanksResult.isLoading ? (
+                <Grid size={{xs:12}}>
+                    <Skeleton variant="rectangular" height="100%" />
                 </Grid>
-            </Box>
+            ) : tanksResult.isError ? (
+                <Grid size={{xs:12}}>
+                    Could not load tank stats: {(tanksResult.error as Error).message}
+                </Grid>
+            ) : tanks && tanks.totals?.battlesAll && tanks.totals?.battlesAll > 500 ? (
+                <> 
+                    <Grid size={{xs:12}} sx={{ textAlign: 'left' }}>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="body2" color="textSecondary">
+                                    total: {tanks.totals?.battlesAll} <br/>
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    most battles tank: {tanks?.data?.at(0)?.all?.battles}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </>
+            ) : (
+                <CardContent>No tank stats found, or not enough battles (500+) {tanks?.totals?.battlesAll}</CardContent>
+            )}
+            </Grid>
         </Container>
     );
 }
