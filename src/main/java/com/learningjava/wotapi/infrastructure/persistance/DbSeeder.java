c94666f6d7050860ca1dd713a47631dbdbf6de77
Jaap van Dijk <dijkvanjaap@hotmail.com>
2025-09-12 15:59:43 +0200

    invalid constructor call

--- src/main/java/com/learningjava/wotapi/infrastructure/persistance/DbSeeder.java
+++ src/main/java/com/learningjava/wotapi/infrastructure/persistance/DbSeeder.java
@@ -107,11 +107,11 @@ public class DbSeeder {
         //TODO: add missing for NA & ASIA
         //TODO Add -64833 research what it is
         var missingVehicles = new ArrayList<>(List.of(
-            new Vehicle(LocalDate.now(), new VehicleKey(17153, RegionType.EU), "Obj. 430B", "Object 430B", "ussr", false, "", "mediumTank", 10, true, null),
-            new Vehicle(LocalDate.now(), new VehicleKey(16913, RegionType.EU), "Waffenträger auf E 100", "Waffenträger auf E 100", "germany", false, "", "AT-SPG", 10, true, null),
-            new Vehicle(LocalDate.now(), new VehicleKey(14337, RegionType.EU), "Object 263B", "Object 263B", "ussr", false, "", "AT-SPG", 10, true, null),
-            new Vehicle(LocalDate.now(), new VehicleKey(12033, RegionType.EU), "SU-122-5", "SU-122-5", "ussr", false, "", "AT-SPG", 9, true, null),
-            new Vehicle(LocalDate.now(), new VehicleKey(56833, RegionType.EU), "T-44-122", "T-44-122", "ussr", true, "", "AT-SPG", 7, true, null)
+            new Vehicle(LocalDate.now(), new VehicleKey(17153, RegionType.EU), "Obj. 430B", "Object 430B", "ussr", false, "", "mediumTank", 10, true, new Vehicle.Images()),
+            new Vehicle(LocalDate.now(), new VehicleKey(16913, RegionType.EU), "Waffenträger auf E 100", "Waffenträger auf E 100", "germany", false, "", "AT-SPG", 10, true, new Vehicle.Images()),
+            new Vehicle(LocalDate.now(), new VehicleKey(14337, RegionType.EU), "Object 263B", "Object 263B", "ussr", false, "", "AT-SPG", 10, true, new Vehicle.Images()),
+            new Vehicle(LocalDate.now(), new VehicleKey(12033, RegionType.EU), "SU-122-5", "SU-122-5", "ussr", false, "", "AT-SPG", 9, true, new Vehicle.Images()),
+            new Vehicle(LocalDate.now(), new VehicleKey(56833, RegionType.EU), "T-44-122", "T-44-122", "ussr", true, "", "AT-SPG", 7, true, new Vehicle.Images())
         ));
 
         vehicleRepository.saveAll(missingVehicles);
--- src/main/java/com/learningjava/wotapi/infrastructure/persistance/entity/worldoftanks/Vehicle.java
+++ src/main/java/com/learningjava/wotapi/infrastructure/persistance/entity/worldoftanks/Vehicle.java
@@ -1,6 +1,5 @@
 package com.learningjava.wotapi.infrastructure.persistance.entity.worldoftanks;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
