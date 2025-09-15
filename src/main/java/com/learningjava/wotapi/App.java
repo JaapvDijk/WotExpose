7992e2d3276248583757f77afceb7f3bccb57bcf
Jaap van Dijk <dijkvanjaap@hotmail.com>
2025-09-15 23:04:30 +0200

    use logger in app

--- src/main/java/com/learningjava/wotapi/App.java
+++ src/main/java/com/learningjava/wotapi/App.java
@@ -1,5 +1,8 @@
 package com.learningjava.wotapi;
 
+import com.learningjava.wotapi.application.importer.TomatoImporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,10 +13,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class App {
 
-    public static void main(String[] args) {
-        var context = SpringApplication.run(App.class, args);
+    private static final Logger logger = LoggerFactory.getLogger(TomatoImporter.class);
 
-        System.out.println("WotExpose app started");
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+
+        logger.info("app started");
         //CHECK:https://blog.tericcabrel.com/handle-database-migrations-in-a-springboot-application-with-flyway/
     }
 }
